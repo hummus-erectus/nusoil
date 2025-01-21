929178f20e07ecd55884155725dede16d1c87a60
Robert Grayson <robert.samuel.grayson@gmail.com>
2025-01-21 10:56:02 +0900

    Adjust typography in style tab to reflect both main fonts

--- src/components/typography.tsx
+++ src/components/typography.tsx
@@ -9,17 +9,35 @@ export const Typography = () => {
     <>
       <Title text="Typography" />
       <View className="mb-4 flex-col">
-        <Text className="text-3xl  tracking-tight">
-          H1: Lorem ipsum dolor sit
+        {/* Poppins Font Sizes */}
+        <Text className="font-poppins-bold text-3xl tracking-tight">
+          H1 (Poppins Bold): Lorem ipsum dolor sit
         </Text>
-        <Text className="text-2xl ">H2: Lorem ipsum dolor sit</Text>
-        <Text className="text-xl ">H3: Lorem ipsum dolor sit</Text>
-        <Text className="text-lg ">H4: Lorem ipsum dolor sit</Text>
-        <Text className="text-base">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque quasi
-          aut, expedita tempore ratione quidem in, corporis quia minus et
-          dolorem sunt temporibus iusto consequatur culpa. Omnis sequi debitis
-          recusandae?
+        <Text className="font-poppins-semibold text-2xl">
+          H2 (Poppins SemiBold): Lorem ipsum dolor sit
+        </Text>
+        <Text className="font-poppins-medium text-xl">
+          H3 (Poppins Medium): Lorem ipsum dolor sit
+        </Text>
+        <Text className="font-poppins-regular text-lg">
+          H4 (Poppins Regular): Lorem ipsum dolor sit
+        </Text>
+
+        {/* Lora Font Sizes */}
+        <Text className="font-lora text-3xl tracking-tight">
+          H1 (Lora): Lorem ipsum dolor sit
+        </Text>
+        <Text className="font-lora text-2xl">
+          H2 (Lora): Lorem ipsum dolor sit
+        </Text>
+        <Text className="font-lora text-xl">
+          H3 (Lora): Lorem ipsum dolor sit
+        </Text>
+        <Text className="font-poppins-light text-base">
+          Body Text (Poppins Light): Lorem ipsum dolor sit amet consectetur,
+          adipisicing elit. Cumque quasi aut, expedita tempore ratione quidem
+          in, corporis quia minus et dolorem sunt temporibus iusto consequatur
+          culpa. Omnis sequi debitis recusandae?
         </Text>
       </View>
     </>
