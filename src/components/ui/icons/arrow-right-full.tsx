712d9a511233a98e8cdd57b4a67daffd253e12a0
Robert Grayson <robert.samuel.grayson@gmail.com>
2025-01-29 00:25:41 +0900

    Update sizing of arrow SVGs

--- src/components/ui/icons/arrow-right-full.tsx
+++ src/components/ui/icons/arrow-right-full.tsx
@@ -4,9 +4,9 @@ import Svg, { Path } from 'react-native-svg';
 
 export function ArrowRightFull({ color = '#000', ...props }: SvgProps) {
   return (
-    <Svg width={24} height={24} viewBox="0 0 24 24" fill="none" {...props}>
+    <Svg width={18} height={18} viewBox="0 0 18 18" fill="none" {...props}>
       <Path
-        d="M12 18L18 12L12 6"
+        d="M12 15L18 9L12 3"
         stroke={color}
         strokeWidth="1.5"
         strokeMiterlimit="10"
@@ -14,7 +14,7 @@ export function ArrowRightFull({ color = '#000', ...props }: SvgProps) {
         strokeLinejoin="round"
       />
       <Path
-        d="M3 12H18"
+        d="M3 9H18"
         stroke={color}
         strokeWidth="1.5"
         strokeMiterlimit="10"
