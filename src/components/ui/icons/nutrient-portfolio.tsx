3580e1fefb6efae3905e03ea25d1268b4d49e991
Robert Grayson <robert.samuel.grayson@gmail.com>
2025-02-06 00:10:53 +0900

    Resize icon

--- src/components/ui/icons/nutrient-portfolio.tsx
+++ src/components/ui/icons/nutrient-portfolio.tsx
@@ -2,13 +2,11 @@ import * as React from 'react';
 import type { SvgProps } from 'react-native-svg';
 import Svg, { Path } from 'react-native-svg';
 
-// TODO: Replace svg
-
 export function NutrientPortfolio({ color = '#000', ...props }: SvgProps) {
   return (
     <Svg
-      width={24}
-      height={24}
+      width={20}
+      height={20}
       viewBox="0 0 24 24"
       fill="none"
       {...props}
