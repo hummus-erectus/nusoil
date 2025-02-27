9cc1a853b6caf7b3e4436faafdc6416604faf647
Robert Grayson <robert.samuel.grayson@gmail.com>
2025-02-27 14:10:24 +0900

    Update mock

--- __mocks__/react-native-gesture-handler.ts
+++ __mocks__/react-native-gesture-handler.ts
@@ -1,14 +1,44 @@
 // Mock for react-native-gesture-handler
-const mockGestureHandler = jest.requireActual(
-  'react-native-gesture-handler/src/mocks.ts'
-);
+import { View } from 'react-native';
 
-// Mock ScrollView component
-mockGestureHandler.ScrollView = function ScrollView(
-  _props: Record<string, unknown>
-) {
-  const { ScrollView } = require('react-native');
-  return ScrollView;
+// Create a basic mock for the entire module
+const mockGestureHandler = {
+  // Add any specific handlers that are used in your app
+  Swipeable: () => null,
+  DrawerLayout: () => null,
+  State: {
+    UNDETERMINED: 0,
+    FAILED: 1,
+    BEGAN: 2,
+    CANCELLED: 3,
+    ACTIVE: 4,
+    END: 5,
+  },
+  ScrollView: (_props: Record<string, unknown>) => View,
+  Slider: () => null,
+  Switch: () => null,
+  TextInput: () => null,
+  ToolbarAndroid: () => null,
+  ViewPagerAndroid: () => null,
+  DrawerLayoutAndroid: () => null,
+  WebView: () => null,
+  NativeViewGestureHandler: () => null,
+  TapGestureHandler: () => null,
+  FlingGestureHandler: () => null,
+  ForceTouchGestureHandler: () => null,
+  LongPressGestureHandler: () => null,
+  PanGestureHandler: () => null,
+  PinchGestureHandler: () => null,
+  RotationGestureHandler: () => null,
+  /* Buttons */
+  RawButton: () => null,
+  BaseButton: () => null,
+  RectButton: () => null,
+  BorderlessButton: () => null,
+  /* Other */
+  FlatList: () => null,
+  gestureHandlerRootHOC: () => null,
+  Directions: {},
 };
 
 module.exports = mockGestureHandler;
